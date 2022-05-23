681d6e2a4606b6fcbf8d164d4ff1933d05ea1175
zaak <zaak.kerstetter@gmail.com>
2022-05-23 10:27:02 -0600

    commit tree 63259ad2948469bc832ba1dd6c4c6ff057b4ece2

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Add your code here
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+// }
+
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json",
+//     },
+//     body: JSON.stringify(formData)
+// }
+
+// fetch("http://localhost:3000/dogs", configurationObject)
+//     .then(function (response){
+//         return response.json()
+//     })
+//     .then(function(object){
+//         console.log(object)
+//     })
+//     .catch(function(error){
+//         alert("Bad things!")
+//     })
+const user = {
+    name: "Steve",
+    email: "steve@steve.com",
+}
+
+const configObj = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify(user)
+}
+
+function submitData(){
+    return fetch("http://localhost:3000/users", configObj)
+    .then(function (response){
+        return response.json()
+    })
+    .then(function (object){
+    document.body.innerHTML = object ["id"]
+    })
+    .catch(function(error){
+        document.body.innerHTML = error.message
+
+    })
+}
